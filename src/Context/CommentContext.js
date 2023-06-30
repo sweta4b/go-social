91f435a85977c97076530e1ac1685a93549b7461
Sweta <swetabisht4b@outlook.com>
2023-07-01 01:26:45 +0530

    update file

---
+++ src/Context/CommentContext.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { createContext, useContext } from "react";
+import { useAuth } from "./AuthContext";
+import { usePost } from "./PostContext";
+
+
+
+
+const CommentContext = createContext();
+
+export const CommentProvider = ({children}) => {
+    const {token} = useAuth();
+    const {dataDispatch} = usePost()
+
+
+    const addComment = async (postId, comment) => {
+        try {
+            const {data,status} = await axios.post(
+                `/api/comments/add/${postId}`,
+                {
+                    commentData : comment
+                },
+                {
+                    headers: {authorization: token}
+                })
+                if(status === 200 || status ===201){
+                    dataDispatch({type:'get_post', payload: data?.posts})
+                }
+                
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const editComment = async (postId, commentId, comment) => {
+        try {
+            const {data,status} = await axios.post(
+                `/api/comments/edit/${postId}/${commentId}`,
+                {
+                    commentData : comment
+                },
+                {
+                    headers: {authorization: token}
+                })
+                if(status === 200 || status ===201){
+                    dataDispatch({type:'get_post', payload: data?.posts})
+                }
+                
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const deleteComment = async (postId, commentId) => {
+        try {
+            const {data, status} = await axios({
+                method:'POST',
+                url: `/api/comments/delete/${postId}/${commentId}`,
+                headers: {authorization:token}
+            })
+            if(status === 200 || status === 201){
+                dataDispatch({type:'get_post', payload: data?.posts})
+            }
+        } catch (error) {
+           console.log(error) 
+        }
+    }
+
+
+    return(
+        <CommentContext.Provider
+        value={{addComment, editComment, deleteComment}}
+        >
+            {children}
+        </CommentContext.Provider>
+    )
+
+}
+
+export const useComment = () => useContext(CommentContext)
